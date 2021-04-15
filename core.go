78968b0f93c7c0ef2ccfd58217043f897d040750
Arlandis Word <arlandis@remind101.com>
2021-04-15 13:54:53 -0400

    Implement signal handling
    The strategy is described in comments but is a little gross. Basically,
    there's a global signal handler and it needs to be smart enough to
    determine whether the user's intent is to kill a subprocess or the
    foreground shell. This is implemented via some shared global vars that
    are manipulated via function calls...gross, I know.

--- core.go
+++ core.go
@@ -16,6 +16,23 @@ const entryPrompt = "Valar Morghulis\nType 'exit' at any time to quit the shell.
 const CHANGE_DIR = "cd"
 const EXIT = "exit"
 
+// currentCmd is set by 'handleCommand' and is leveraged by the signal handler
+// to determine whether the signal should be propagated to a subprocess or
+// the foreground shell.
+// I don't love this shared state approach, so may rethink it.
+var currentCmd *exec.Cmd = nil
+
+func signalHandler(sigChan <-chan os.Signal) {
+	for {
+		sig := <-sigChan
+		if currentCmd != nil { // kill the currently executing subprocess
+			currentCmd.Process.Signal(sig)
+		} else { // kill the foreground
+			os.Exit(1)
+		}
+	}
+}
+
 func handleCommand(s string) error {
 	parts := strings.Split(s, " ")
 	switch parts[0] {
@@ -25,23 +42,18 @@ func handleCommand(s string) error {
 		err := os.Chdir(parts[1])
 		return err
 	default:
-		sigChan := make(chan os.Signal, 1)
-		signal.Notify(sigChan, os.Interrupt)
 		cmd := exec.Command(parts[0], parts[1:]...)
-		go func() {
-			sig := <-sigChan
-			cmd.Process.Signal(sig)
-		}()
+		currentCmd = cmd
 		out, err := cmd.Output()
+		currentCmd = nil
 		// -1 means either the user sent an interrupt signal or the child process isn't done yet
 		// In our case it *must* be the former since using 'Output' waits. So if we reach
 		// this line then execution has stopped.
 		// There's a defensive cond check just in case there's something I'm missing.
-		if cmd.ProcessState == nil {
-			fmt.Println("Something went wrong. This shouldn't happen.")
-			return nil
-		}
 		if cmd.ProcessState.ExitCode() == -1 {
+			if cmd.ProcessState == nil {
+				fmt.Println("Something went wrong. This shouldn't happen.")
+			}
 			return nil
 		}
 		if err != nil {
@@ -57,6 +69,9 @@ var commandFlag = flag.String("c", "", "Run a command in a shell subprocess and 
 
 func main() {
 	flag.Parse()
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt)
+	go signalHandler(sigChan)
 	if *commandFlag != "" {
 		handleCommand(*commandFlag)
 	} else {
